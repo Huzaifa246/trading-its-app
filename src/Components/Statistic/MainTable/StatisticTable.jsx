d93f8c8c63f3ec665fe7e81695eee76b51c859e3
Huzaifa246 <huzaifa24680@gmail.com>
2023-09-01 17:19:34 +0500

    mr added

--- src/Components/Statistic/MainTable/StatisticTable.jsx
+++ src/Components/Statistic/MainTable/StatisticTable.jsx
@@ -101,7 +101,9 @@ function StatisticTable({ optionId }) {
                 }}>
                 </div>
                 <div>
-                    <h1 className="h1-deposit">Add Investment</h1>
+                    <h1 className="h1-deposit" style={{
+                        marginTop: "20px"
+                    }}>Add Investment</h1>
                     <div>
                         <div className="range-value">
                             <input
